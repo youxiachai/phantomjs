d84d3ec83f507480381d9e60afd67478d4167197
youxiachai <youxiachai@gmail.com>
2014-03-20 12:32:06 +0800

    change url

--- install.js
+++ install.js
@@ -21,7 +21,7 @@ var url = require('url')
 var util = require('util')
 var which = require('which')
 
-var downloadUrl = 'http://cdn.bitbucket.org/ariya/phantomjs/downloads/phantomjs-' + helper.version + '-'
+var downloadUrl = 'http://nodephantomjs.qiniudn.com/phantomjs-' + helper.version + '-'
 
 var originalPath = process.env.PATH
 
